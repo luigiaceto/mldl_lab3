ac8fae9aba5d4b3d8f605202c228905f3be1743c
Luigi Aceto <luigiaceto32@gmail.com>
2025-11-04 22:55:04 +0100

    ...

--- dataset/dataloader.py
+++ dataset/dataloader.py
@@ -1,5 +1,5 @@
 import torch
-from dataset.dataset import getDataset
+from .dataset import getDataset
 
 def getDataLoader(train_path, val_path, batch_size):
   train, val = getDataset(train_path, val_path)
